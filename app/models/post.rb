25b8662d292f4e46f023a54d807e5d9499d6520b
Elle Faraday <faraday.elle@gmail.com>
2018-07-08 21:04:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
 
   def categories_attributes=(category_attributes)
     category_attributes.each do |catergory_attribute|
-      category = Category.find_or_create(catergory_attribute)
+      category = Category.find_or_create_by(catergory_attribute)
       self.categories << category
     end
   end
