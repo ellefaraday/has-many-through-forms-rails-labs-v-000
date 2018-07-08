06e1d040091b5eb9ad9dc62e4f232a34ca7e5330
Elle Faraday <faraday.elle@gmail.com>
2018-07-08 21:48:09 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :users
+  #accepts_nested_attributes_for :users
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |catergory_attribute|
@@ -12,10 +12,17 @@ class Post < ActiveRecord::Base
     end
   end
 
-  def user_attributes=(user_attributes)
-    user_attributes.values.each do |user_attribute|
-      user = User.find_or_create_by(user_attribute)
-      self.users << user
+  def comments_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comments << comment
     end
   end
+
+  #def user_attributes=(user_attributes)
+  #  user_attributes.values.each do |user_attribute|
+  #    user = User.find_or_create_by(user_attribute)
+  #    self.users << user
+  #  end
+  #end
 end
