f4a09b7d337ce620593660a51c838d17f74e7b69
Elle Faraday <faraday.elle@gmail.com>
2018-07-08 22:26:20 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
-    redirect_to post_path(comment.post)
+    redirect_to post_path(commlent.post)
   end
 
   private
