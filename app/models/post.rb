3ce31f3626a0399911f127b421b254b7244208ed
Elle Faraday <faraday.elle@gmail.com>
2018-07-08 21:43:02 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,19 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :users
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |catergory_attribute|
       category = Category.find_or_create_by(catergory_attribute)
       self.categories << category
+    end
+  end
+
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.users << user
     end
   end
 end
