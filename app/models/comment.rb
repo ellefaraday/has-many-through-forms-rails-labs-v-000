f1de1e924d0c76b841590f83e8f2c7d7fbe50a1f
Elle Faraday <faraday.elle@gmail.com>
2018-07-08 22:36:41 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ActiveRecord::Base
 
   def user_attributes=(user_attributes)
     if user_attributes[:username] != ""
-      user = User.find_or_create_by(user_attributes[:username])
+      user = User.find_or_create_by(username: user_attributes[:username])
       self.user = user
     end
   end
